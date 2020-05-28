9671c99476088db95950b2df6e980dc6ede99293
Thanh Pham <tpham0123@gmail.com>
2020-05-28 20:52:45 +0000

    Automatically backed up by Learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -18,16 +18,11 @@ class Transfer
       @sender.balance -= @amount
       @receiver.balance += @amount
       @status = "complete"
-     else
-      @status = "rejected"
-      
-    end 
-    if @sender.balance < @amount
+    else
       @status = "rejected"
       return "Transaction rejected. Please check your account balance."
-    end 
-
-  end 
+    end
+  end
   
    
 end
