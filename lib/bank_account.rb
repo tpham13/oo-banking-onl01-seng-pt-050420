f07710c16291e51e24e255bb7ecce6a69605dfb0
Thanh Pham <tpham0123@gmail.com>
2020-05-28 19:26:37 +0000

    Done.

--- lib/bank_account.rb
+++ lib/bank_account.rb
@@ -1,16 +1,27 @@
 class BankAccount
-  attr_accessor :name 
+  attr_accessor :balance, :status
+  attr_reader :name   # this make sure that the name can only but read but not changed
   
   def initialize(name)
      @name = name 
-  end 
-
-  def balance(amount = 1000)
-    @amount = amount 
-  end 
-
-  def status(status = "open")
-    @status = status 
+     @balance = 1000
+     @status = "open"
+   
   end 
   
+  def deposit (money)
+    @balance += money
+  end 
+  
+  def display_balance
+   "Your balance is $#{balance}."
+  end 
+  
+  def valid? 
+    @status == "open" && @balance > 0? true : false 
+  end 
+  
+  def close_account
+    @status = "closed"
+  end 
 end
