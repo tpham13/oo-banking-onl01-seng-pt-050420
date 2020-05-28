4a711781d5e05cecf05a2d63ffc532594c53b051
Thanh Pham <tpham0123@gmail.com>
2020-05-28 17:28:41 +0000

    Done.

--- lib/bank_account.rb
+++ lib/bank_account.rb
@@ -1,3 +1,16 @@
 class BankAccount
+  attr_accessor :name 
+  
+  def initialize(name)
+     @name = name 
+  end 
 
+  def balance(amount = 1000)
+    @amount = amount 
+  end 
+
+  def status(status = "open")
+    @status = status 
+  end 
+  
 end
